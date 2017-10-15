47768cd6e460bc7555a8ca111b0742fc33c5d6f5
Shinji16081989 <dang.nguyen16081989@gmail.com>
2017-10-15 22:56:40 +0700

    add them bai

---
+++ Tu Duy/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int diemToan;
+    cout << "Nhap vao diem Toan: ";
+    cin >> diemToan;
+    cout << endl;
+    cout << "Diem toan ban vua nhap la: " << diemToan << endl;
+}
