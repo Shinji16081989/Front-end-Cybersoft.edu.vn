8a4d4dfb9250f741372a66d939244c3a913cf6dc
Nguyen Hai Dang <dang.nguyen16081989@gmail.com>
2017-11-09 18:08:13 +0700

    add TuDuy

---
+++ TuDuy/Baitapbuoi3/Bai1/Project1/Project1/Source.cpp
@@ -0,0 +1,26 @@
+﻿/* Một hộp sữa có thể chứa 3,78 lít sữa. Mỗi buổi sáng, một trang trại bò sữa đưa các hộp sữa đến một cửa hàng tạp hoá tại địa phương. Chi phí sản xuất một lít sữa là 0,38 đô la, 
+và lợi nhuận của mỗi hộp sữa là 0,27 đô la. Viết chương trình thực hiện những việc sau:
+
+a. Nhắc người dùng nhập tổng số lượng sữa được sản xuất vào buổi sáng.
+b. Xuất ra số lượng hộp sữa cần thiết để giữ sữa. (Đưa ra câu trả lời cho số nguyên gần nhất.)
+c. Xuất ra chi phí sản xuất sữa.
+d. Xuất ra lợi nhuận ước tính của số lượng sữa đã sản xuất.
+*/
+#include <iostream>
+
+using namespace std;
+
+const double ONE_BOX = 3.78;
+const double FEE_PER_LITER = 0.38;
+const double INCOME_PER_BOX = 0.27;
+
+int main()
+{
+	int milkorder;
+	int boxperday;
+	double feetoproduct;
+	double income;
+
+
+	
+}
---
+++ TuDuy/Baitapbuoi3slide/Project1/UnlockPDF.cpp
@@ -0,0 +1,23 @@
+﻿/*Giải bài tập mở khóa file pdf*/
+
+#include <iostream>
+
+using namespace std;
+
+const int PRIME = 71;
+const int ONE = 1;
+int main()
+{
+	int count, sum;
+	double x;
+		count = 1;
+	sum = count + PRIME;
+	x = 25.67;
+		sum = sum + count;
+	x = sum++;
+	x = x + sum *count;
+	sum += 3;
+	cout << " count = " << count << ", sum = " << sum << ", PRIME = " << PRIME << endl;
+
+	system("pause");
+}
