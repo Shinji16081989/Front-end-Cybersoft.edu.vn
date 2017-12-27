a1af6036fa41d71a621c82fdc7bf3f9c3c8ae1aa
Shinji16081989 <dang.nguyen16081989@gmail.com>
2017-12-27 07:51:03 +0700

    add jQuery

--- Jquery/Teamtreehouse/Day3/js/app.js
+++ Jquery/Teamtreehouse/Day3/js/app.js
@@ -6,11 +6,30 @@ const $secureLinks = $('a[href^="https://"]');
 //$secureLinks.hide();
 const $pdfs = $('a[href$=".pdf"]');
 //$pdfs.hide();
+const $pdfCheckbox = $("<label><input type='checkbox'> Allow PDF downloads</label>");
 
 $secureLinks.attr('target', '_blank');
 $pdfs.attr('download', true);
 
-$odd.css('backgroundColor', 'green');
+$odd.css('color', 'green');
 
 $secureLinks.addClass('secure');
 $pdfs.addClass('pdf');
+
+$pdfs.on('click', function(event) {
+  //check if checkbox has been checked
+  // if zero checkboxes are checked
+  if ($(':checked').length === 0){
+    // prevent download of document.
+    // alert the user
+    event.preventDefault();
+    alert('Please check the box to allow PDF downloads.');
+  }
+});
+
+$('#links').append($pdfCheckbox);
+
+$('a').each(function(){
+  const url = $(this).attr('href');
+  $(this).parent().append(`(${url})`);
+});
