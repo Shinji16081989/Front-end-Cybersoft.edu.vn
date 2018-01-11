7ac65a87c59cd7bc51c899d1ec47397c60efb33e
Shinji16081989 <dang.nguyen16081989@gmail.com>
2018-01-11 22:11:45 +0700

    add angular2

--- Angular2/BaitapAngular/day3/MyHomeWork/src/app/loginform/loginform.component.ts
+++ Angular2/BaitapAngular/day3/MyHomeWork/src/app/loginform/loginform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-loginform',
@@ -6,14 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loginform.component.css']
 })
 export class LoginformComponent implements OnInit {
-  public status:boolean = true;
-  DangNhap(user, password)
-  {
-    if (user === "cybersoft" && password === "cybersoft"){
+  public status: boolean = true;
+  DangNhap(user, password) {
+    if (user === "cybersoft" && password === "cybersoft") {
       this.status = false;
-    }else{
+      swal({
+        position: 'center',
+        type: 'success',
+        title: 'Bạn đã đăng nhập thành công',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    } else if(user === "dang" && password === "dang"){
+      this.status = false;
+      swal({
+        title: '<i>Xin Chào</i> <u>Đăng</u>',
+        type: 'info',
+        html:
+        'You can use <b>bold text</b>, ' +
+        '<a href="//github.com">links</a> ' +
+        'and other HTML tags',
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText:
+        '<i class="fa fa-thumbs-up"></i> Great!',
+        confirmButtonAriaLabel: 'Thumbs up, great!',
+        cancelButtonText:
+        '<i class="fa fa-thumbs-down"></i>',
+        cancelButtonAriaLabel: 'Thumbs down',
+      })
+    }else {
       this.status = true;
-      alert("Sai Thong Tin");
+      swal(
+        {
+          position: 'center',
+          type: 'warning',
+          title: 'Nhập sai thông tin',
+          showConfirmButton: false,
+          timer: 1500
+        }
+      );
     }
   }
   constructor() { }
